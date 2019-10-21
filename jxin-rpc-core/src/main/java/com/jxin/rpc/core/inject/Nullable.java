e771640030c85779d0f568d5d791ebefae7fadf3
Jxin <105760730@qq.com>
2019-10-21 22:04:17 +0800

    添加可以为null的注解

---
+++ jxin-rpc-core/src/main/java/com/jxin/rpc/core/inject/Nullable.java
@@ -0,0 +1,15 @@
+package com.jxin.rpc.core.inject;
+
+import java.lang.annotation.*;
+
+/**
+ * 可以为null的注解
+ * @author 蔡佳新
+ * @version 1.0
+ * @since 2019/10/21 21:21
+ */
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Nullable {
+}
