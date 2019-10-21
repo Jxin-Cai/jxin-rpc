1eb8476042a8432153410809572272828d23cbd9
Jxin <105760730@qq.com>
2019-10-21 22:04:31 +0800

    字符串序列化工具

---
+++ jxin-rpc-core/src/main/java/com/jxin/rpc/core/serializer/impl/StringSerializer.java
@@ -0,0 +1,45 @@
+package com.jxin.rpc.core.serializer.impl;
+
+import com.jxin.rpc.core.serializer.DataEnum;
+import com.jxin.rpc.core.serializer.Serializer;
+import org.apache.commons.lang3.StringUtils;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 字符串序列化实现类
+ * @author 蔡佳新
+ * @version 1.0
+ * @since 2019/10/21 21:23
+ */
+public class StringSerializer implements Serializer<String> {
+
+    @Override
+    public void serialize(String entry, byte[] bytes, int offset, int length) {
+        if(StringUtils.isBlank(entry)){
+            return ;
+        }
+         final byte [] dataBytes = entry.getBytes(StandardCharsets.UTF_8);
+         System.arraycopy(dataBytes, 0, bytes, offset, dataBytes.length);
+    }
+
+    @Override
+    public String deserialize(byte[] bytes, int offset, int length) {
+        return new String(bytes, offset, length, StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public Integer size(String entry) {
+        return entry.getBytes(StandardCharsets.UTF_8).length;
+    }
+
+    @Override
+    public DataEnum type() {
+        return DataEnum.STR;
+    }
+
+    @Override
+    public Class<String> getSerializeClass() {
+        return String.class;
+    }
+}
